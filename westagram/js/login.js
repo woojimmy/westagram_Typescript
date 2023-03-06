eedba53d337885eb3e1e66b7898b880a441df71c
woojimmy <lwj6079@gmail.com>
2023-03-06 21:27:36 +0900

    Typescript install

--- westagram/js/login.js
+++ westagram/js/login.js
@@ -1,15 +1,10 @@
-// input ID element
 let loginInputId = document.getElementById("idInput");
-// input password element
 let loginInputPw = document.getElementById("pwInput");
-// login button element
 let loginBtnElement = document.getElementById("loginBtn");
 
-// active event - switching button color
-window.addEventListener("keyup", () => {
+document.addEventListener("keyup", () => {
   let loginInputIDField = loginInputId.value;
   let loginInputPWField = loginInputPw.value;
-  /// check exist of `@` in input field
   const checkEMail = loginInputIDField.indexOf("@");
 
   if (loginInputIDField.length > 0 && loginInputPWField.length > 4) {
@@ -27,25 +22,25 @@ window.addEventListener("keyup", () => {
 });
 
 // click event - pop up alert
-loginBtnElement.addEventListener('click', function(e) {
+loginBtnElement.addEventListener("click", function (e) {
   e.preventDefault();
   let loginInputIDField = loginInputId.value;
   let loginInputPWField = loginInputPw.value;
 
   const checkEMail = loginInputIDField.indexOf("@");
-  if(loginInputIDField.length == 0) {
-    alert('사용자 이메일을 입력해주세요.')
-  } else if(checkEMail == -1) {
-    alert('올바른 이메일을 입력해주세요.')
-  } else if(loginInputPWField.length == 0) {
-    alert('비밀번호를 입력해주세요.')
-  } else if(loginInputPWField.length < 5) {
-    alert('비밀번호는 5자 이상입니다. 다시 입력해주세요.')
+  if (loginInputIDField.length == 0) {
+    alert("사용자 이메일을 입력해주세요.");
+  } else if (checkEMail == -1) {
+    alert("올바른 이메일을 입력해주세요.");
+  } else if (loginInputPWField.length == 0) {
+    alert("비밀번호를 입력해주세요.");
+  } else if (loginInputPWField.length < 5) {
+    alert("비밀번호는 5자 이상입니다. 다시 입력해주세요.");
   } else {
-    loginToMainPage();  }
-})
-
+    loginToMainPage();
+  }
+});
 
 const loginToMainPage = () => {
-  window.location.href = '/students/40th/woojin/main.html';
-}
+  window.location.href = "/students/40th/woojin/main.html";
+};
